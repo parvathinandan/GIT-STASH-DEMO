e0019eff3bd192509be78b4a3716eae397c4cafb
parvathinandan <61867180+parvathinandan@users.noreply.github.com>
2021-05-10 17:38:28 +0530

    my stash commit

--- src/main/java/com/example/admin/MyOwnStashDemo.java
+++ src/main/java/com/example/admin/MyOwnStashDemo.java
@@ -2,7 +2,11 @@ package com.example.admin;
 
 public class MyOwnStashDemo {
 
+	private Double num = 657.54;
+	
 	private Integer eno = 1 ;
+	
+	private String msg = "message";
 	
 	public static void Method1() {
 		System.out.println("i am method1() invoking duty");
