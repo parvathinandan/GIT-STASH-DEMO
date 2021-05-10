60429a1a3a421c1bd1ac20bedc64445a50913d80
parvathinandan <61867180+parvathinandan@users.noreply.github.com>
2021-05-10 17:30:24 +0530

    stash purpose commit

---
+++ src/main/java/com/example/admin/GitStashCommandDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitStashCommandDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitStashCommandDemoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/admin/MyOwnStashDemo.java
@@ -0,0 +1,10 @@
+package com.example.admin;
+
+public class MyOwnStashDemo {
+
+	private Integer eno = 1 ;
+	
+	public static void Method1() {
+		System.out.println("i am method1() invoking duty");
+	}
+}
